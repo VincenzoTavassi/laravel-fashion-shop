19e08448db8f4896e4a6b88672553fa3f33df829
VincenzoTavassi <info@egadivacanze.it>
2023-04-14 15:12:19 +0200

    fix db seeder typo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(
-            ShoesSeeder::class,
+            ShoeSeeder::class,
             UserSeeder::class,
         );
     }
