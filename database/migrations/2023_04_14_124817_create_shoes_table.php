38811d83cbc566f519abae41e2c7d598bed7d5cb
VincenzoTavassi <info@egadivacanze.it>
2023-04-14 15:10:50 +0200

    create migration

---
+++ database/migrations/2023_04_14_124817_create_shoes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 50);
+            $table->string('model', 100);
+            $table->string('material', 100)->nullable();
+            $table->string('color', 20);
+            $table->string('number');
+            $table->decimal('price', 6, 2);
+            $table->text('description')->nullable();
+            $table->boolean('is_available')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shoes');
+    }
+};
