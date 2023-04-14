5cfd82f9c22a49d78bac7c2391821511b6cbeab6
MartinaPerozzi <perozzimartina@gmail.com>
2023-04-14 18:31:21 +0200

    layout- destroy

--- resources/views/admin/shoes/index.blade.php
+++ resources/views/admin/shoes/index.blade.php
@@ -1,39 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
-  <section class="container">
+    <section class="container">
 
-    <h1 class="my-3">Scarpe</h1>
-    <a href="{{ route('admin.shoes.create') }}" class="btn btn-primary">Crea</a>
+        <h1 class="my-3">Scarpe</h1>
+        <a href="{{ route('admin.shoes.create') }}" class="btn btn-primary">Crea</a>
 
-    <table class="table my-3">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Brand</th>
-          <th scope="col">Modello</th>
-          <th scope="col">Prezzo</th>
-          <th scope="col">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        @forelse($shoes as $shoe)
-          <tr>
-            <th scope="row">{{ $shoe->id }}</th>
-            <td>{{ $shoe->brand }}</td>
-            <td>{{ $shoe->model }}</td>
-            <td>€ {{ $shoe->price }}</td>
-            <td>
-              <a href="{{ route('admin.shoes.show', $shoe) }}">Dettaglio</a>
-            </td>
-          </tr>
-        @empty
-          <h2>Non ci sono scarpe</h2>
-        @endforelse
-      </tbody>
-    </table>
+        <table class="table my-3">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Brand</th>
+                    <th scope="col">Modello</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($shoes as $shoe)
+                    <tr>
+                        <th scope="row">{{ $shoe->id }}</th>
+                        <td>{{ $shoe->brand }}</td>
+                        <td>{{ $shoe->model }}</td>
+                        <td>€ {{ $shoe->price }}</td>
+                        <td>
+                            <a href="{{ route('admin.shoes.show', $shoe) }}"><i class="fa-solid fa-eye"></i></a>
+                            <a href="{{ route('admin.shoes.edit', $shoe) }}"><i
+                                    class="fa-solid fa-pen-to-square ms-3"></i></a>
+                            <button class="fa-solid fa-trash btn-icon ms-3 text-danger" data-bs-toggle="modal"
+                                data-bs-target="#delete-modal-{{ $shoe->id }}">
+                                {{-- <i class="fa-solid fa-trash"></i> --}}
+                            </button>
+                        </td>
+                    </tr>
+                @empty
+                    <h2>Non ci sono scarpe</h2>
+                @endforelse
+            </tbody>
+        </table>
 
-    {{ $shoes->links() }}
+        {{ $shoes->links() }}
 
-  </section>
+    </section>
+@endsection
+
+@section('modals')
+    @foreach ($shoes as $shoe)
+        <!-- Modal -->
+        <div class="modal fade" id="delete-modal-{{ $shoe->id }}" tabindex="-1" data-bs-backdrop="static"
+            aria-labelledby="delete-modal-{{ $shoe->id }}-label" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="delete-modal-{{ $shoe->id }}-label">Conferma eliminazione</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-start">
+                        Sei sicuro di voler eliminare la canzone <strong>{{ $shoe->title }}</strong> con ID
+                        <strong> {{ $shoe->id }}</strong>? <br>
+                        L'operazione non è reversibile!
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+
+                        <form action="{{ route('admin.shoes.destroy', $shoe) }}" method="POST" class="">
+                            @method('delete')
+                            @csrf
+
+                            <button type="submit" class="btn btn-danger">Elimina</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,27 +2,39 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ env('APP_NAME', 'Laravel') }}</title>
+    <title>{{ env('APP_NAME', 'Laravel') }}</title>
+    {{-- LINK --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <!-- Vite -->
-  @vite(['resources/js/app.js'])
+    <!-- Vite -->
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
 
-  @include('layouts.partials._navbar')
+    @include('layouts.partials._navbar')
 
-  <main>
-    <div class="py-5">
-      @yield('content')
-    </div>
-  </main>
+    <main>
+        <div class="py-5">
+            <div class="container">
+                @if (session('message'))
+                    <div class="alert alert-{{ session('message-type') ? session('message-type') : 'success' }}">
+                        {{ session('message') }}</div>
+                @endif
+
+            </div>
+            @yield('content')
+        </div>
+    </main>
+    @yield('modals')
 </body>
 
 </html>
