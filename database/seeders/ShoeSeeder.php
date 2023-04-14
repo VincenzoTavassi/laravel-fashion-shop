b8b72530f43e8a40fec33e03a58789b381756218
VincenzoTavassi <info@egadivacanze.it>
2023-04-14 15:11:13 +0200

    seeders

---
+++ database/seeders/ShoeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Shoe;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ShoeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $shoe = new Shoe;
+            $shoe->brand = $faker->company();
+            $shoe->model = $faker->streetName();
+            $shoe->material = $faker->word();
+            $shoe->color = $faker->colorName();
+            $shoe->number = $faker->numberBetween(35, 49);
+            $shoe->price = $faker->randomFloat(2, 10, 9999);
+            $shoe->description = $faker->paragraph(5);
+            $shoe->is_available = $faker->boolean();
+            $shoe->save();
+        }
+    }
+}
