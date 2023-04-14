56e8e4383e5daea189a0904fb00897a0f8272c45
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-04-14 17:05:45 +0200

    add show

---
+++ resources/views/admin/shoes/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+  {{-- @dump($shoe) --}}
+  <div class="container">
+    <h1 class="my-3">{{ $shoe->brand }} - {{ $shoe->model }}</h1>
+
+    <a href="{{ route('admin.shoes.index') }}" class="btn btn-primary my-3">Torna alla lista</a>
+    <div class="card">
+      <img src="{{ $shoe->image }}" class="card-img-top" alt="Shoe image">
+      <div class="card-body">
+        <h5 class="card-title">{{ $shoe->model }}</h5>
+        <p class="card-text">Brand: {{ $shoe->brand }}</p>
+        <p class="card-text">Materiale: {{ $shoe->material }}</p>
+        <p class="card-text">Colore: {{ $shoe->color }}</p>
+        <p class="card-text">Prezzo: € {{ $shoe->price }}</p>
+        <p class="card-text">Creato il: {{ $shoe->created_at }}</p>
+        <p class="card-text">Aggiornato il: {{ $shoe->updated_at }}</p>
+        <p class="card-text">{{ $shoe->description }}</p>
+
+      </div>
+    </div>
+  </div>
+@endsection
