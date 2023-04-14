e2ac6919ca6e58e344f001bae7dafe55fd5bfe78
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-04-14 16:19:04 +0200

    index refactor

--- resources/views/admin/shoes/index.blade.php
+++ resources/views/admin/shoes/index.blade.php
@@ -1,39 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-
-<section class="container">
+  <section class="container">
 
     <h1 class="my-3">Scarpe</h1>
 
     <table class="table my-3">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Brand</th>
-                <th scope="col">Modello</th>
-                <th scope="col">Prezzo</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($shoes as $shoe)
-            <tr>
-                <th scope="row">{{$shoe->id}}</th>
-                <td>{{$shoe->brand}}</td>
-                <td>{{$shoe->model}}</td>
-                <td>€ {{$shoe->price}}</td>
-                <td>Dettaglio</td>
-            </tr>
-            @empty
-            <h2>Non ci sono scarpe</h2>
-            @endforelse
-        </tbody>
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Modello</th>
+          <th scope="col">Prezzo</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        @forelse($shoes as $shoe)
+          <tr>
+            <th scope="row">{{ $shoe->id }}</th>
+            <td>{{ $shoe->brand }}</td>
+            <td>{{ $shoe->model }}</td>
+            <td>€ {{ $shoe->price }}</td>
+            <td>Dettaglio</td>
+          </tr>
+        @empty
+          <h2>Non ci sono scarpe</h2>
+        @endforelse
+      </tbody>
     </table>
 
-    {{$shoes->links()}}
+    {{ $shoes->links() }}
 
-</section>
-
-
+  </section>
 @endsection
