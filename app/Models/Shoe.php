ba1b2e9a2fd5a83eca99cef1804a8b91aef633a1
VincenzoTavassi <info@egadivacanze.it>
2023-04-15 18:21:54 +0200

    get image function (null, http, storage)

--- app/Models/Shoe.php
+++ app/Models/Shoe.php
@@ -9,13 +9,28 @@ class Shoe extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['brand', 'model', 'material', 'color', 'price', 'description', 'is_available'];
+    protected $fillable = ['brand', 'model', 'material', 'color', 'price', 'description', 'is_available', 'image'];
 
-    protected function getCreatedAtAttribute($value) {
+    // MUTATORS
+    protected function getCreatedAtAttribute($value)
+    {
         return date('d/m/y h:i', strtotime($value));
     }
 
-    protected function getUpdatedAtAttribute($value) {
+    protected function getUpdatedAtAttribute($value)
+    {
         return date('d/m/y h:i', strtotime($value));
+    }
+
+    //GET IMAGE
+    public function getImage()
+    {
+        if (empty($this->image)) { // Se l'immagine è indefinita, ritorna il placeholder
+            return asset('storage/img/placeholder.png');
+        } elseif (str_starts_with($this->image, 'http')) { // Se l'immagine è una URL ritorna la URL
+            return $this->image;
+        } else {
+            return asset('storage/' . $this->image); // Altrimenti usa lo storage
+        }
     }
 }
